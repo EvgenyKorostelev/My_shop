fe76cb6d3753a37fb95f87551e5c5c5dc5c18ade
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-04-26 14:53:19 +0300

    user_authorization_and_authentication_oauth_ for_MyShop

--- worker-app/src/main/java/ru/shop/worker/config/ClientBeans.java
+++ worker-app/src/main/java/ru/shop/worker/config/ClientBeans.java
@@ -3,9 +3,12 @@ package ru.shop.worker.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 import ru.shop.worker.client.RestClientProductsRestClient;
+import ru.shop.worker.security.OAuthClientHttpRequestInterceptor;
 
 @Configuration
 public class ClientBeans {
@@ -13,12 +16,16 @@ public class ClientBeans {
     @Bean
     public RestClientProductsRestClient productsRestClient(
             @Value("${shop.catalogue.service.uri:http://localhost:8081}") String catalogueBaseUri,
-            @Value("${shop.catalogue.service.username:}") String catalogueUsername,
-            @Value("${shop.catalogue.service.password:}") String cataloguePassword){
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${shop.catalogue.service.registration-id:keycloak}") String registrationId) {
+
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
                 .requestInterceptor(
-                        new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
                 .build());
     }
 }
